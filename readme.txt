784e3b55fcfabe47328ed0fe3bc02472d55540c6
EndoEU <70594311+EndoEU@users.noreply.github.com>
2022-05-08 15:27:30 +0100

    added new templates

--- readme.txt
+++ readme.txt
@@ -1,2 +1,2 @@
 video : https://www.youtube.com/watch?v=PtQiiknWUcI&list=WL&index=3
-Time Stamp : 48:34
+Time Stamp : 1:04:49
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+rooms = [
+    {'id' : 1, 'name' : 'Lets learn Django!'},
+    {'id' : 2, 'name' : 'Lets learn Python!'},
+    {'id' : 3, 'name' : 'Lets learn Web Development!'},
+]
 
-def room(request):
-    return render(request, 'room.html')
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room' : room}
+    return render(request, 'base/room.html', context)
